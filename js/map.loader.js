bbd09906a729ac2c09fd21ddeae2fb51815c16a2
Kristian Lundström <kristian.lundstrom@gmail.com>
2014-10-27 18:41:58 +0100

    Testa geolocation

--- js/map.loader.js
+++ js/map.loader.js
@@ -4,7 +4,26 @@
     var infowindow;
     var markers = new Array();
     var infoWindows = new Array();
+    var currentLocation = geolocation.getCurrentPosition();
     map_initialize(); // load map
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geo-info');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          +                                   position.timestamp          + '<br />';
+    }
+
     function map_initialize(){
        
         //Google map option
